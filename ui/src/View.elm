b069a70b0296c7530573b4e40bb22d01809b43a1
Shteevee <shteve451@gmail.com>
2019-12-02 17:12:43 +0000

    Added some style to the page

--- ui/src/View.elm
+++ ui/src/View.elm
@@ -1,26 +1,25 @@
 module View exposing (view)
 
-import Types exposing (Model, Msg, TaskItem)
+import Types exposing (..)
 import Html exposing (Html, Attribute, div, input, text, button, li, ul, label)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
+import Svg exposing (svg, use)
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ ul []
-    (List.concat [
-        List.map toListItem model.toDoList
-    ])
-    , input [ placeholder "Enter a task here...", value model.content, onInput Types.Change ] []
-    , button [ onClick (Types.SubmitTask model.content) ] [ text "Add Task" ]
+  div [ class "FF-Container" ]
+    [
+    ul [ class "FF-List" ] (List.concat [ List.map toListItem model.toDoList ]),
+    input [ class "FF-Text-Input", placeholder "Enter a task here...", value model.content, onInput Change ] [],
+    button [ class "FF-Submit-Button", onClick (SubmitTask model.content) ] [ text "Add Task" ]
     ]
 
 toListItem: TaskItem -> Html Msg
 toListItem (id, item) =
-  li []
-     [
-     input [type_ "checkbox"] [],
-     label [][text item],
-     button [ onClick (Types.RemoveTask id) ] [ text (String.fromInt id) ]
-     ]
+  li [ class "FF-List-Element" ]
+    [
+    input [ type_ "checkbox" ] [],
+    label [][ text item ],
+    button [ onClick (RemoveTask id) ] [ text "X" ]
+    ]
